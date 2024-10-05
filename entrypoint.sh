bdc3e55a1d8653942a5f5794852950e6b1006c9d
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-05 12:55:30 -0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#echo "VITE_API_URL=${VITE_API_URL:-http://localhost:3000}" > /usr/src/app/.env.production
 echo "El valor de VITE_API_URL es: $VITE_API_URL"
 echo "VITE_API_URL=$VITE_API_URL" > /usr/src/app/.env.production
 
