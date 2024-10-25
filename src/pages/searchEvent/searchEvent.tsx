f5ef893b2e99f584bfa728bf5a5754f6e5067213
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-24 22:41:38 -0500

    fix

--- src/pages/searchEvent/searchEvent.tsx
+++ src/pages/searchEvent/searchEvent.tsx
@@ -16,6 +16,8 @@ import Utils from "../../utils/utils";
 import styles from "./searchEvent.module.css";
 import { MySeo } from "../../components/mySeo/mySeo";
 import { store } from "../../store/store";
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css';
 
 const SearchEvent = () => {
     const dispatch = useDispatch();
@@ -28,28 +30,28 @@ const SearchEvent = () => {
     const imageNumber = useMemo(() => Utils.generateRandom(3, 9), []);
 
     useEffect(() => {
-        loadingModal.open({title: "Cargando los eventos"});
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        loadingModal.open({ title: "Cargando los eventos" });
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
         onLoadData();
     }, []);
 
     const onLoadData = async () => {
         var searchText = store.getState().search.search;
         setSearch(searchText);
-        var res:any = await searchEvents(searchText, "date", ASC_ORDER_BY)
+        var res: any = await searchEvents(searchText, "date", ASC_ORDER_BY)
         setEvents(res.data);
         loadingModal.close();
     }
 
     const inputChanged = (text: any) => {
         text = String(text).trim();
-        
+
         setSearch(text);
         clearTimeout(timer);
 
-        const newTimer = setTimeout(async() => {
+        const newTimer = setTimeout(async () => {
             dispatch(onSearch(text));
-            searchEvents(text, "date", (orderByDate) ? DESC_ORDER_BY : ASC_ORDER_BY).then((res:any) => {
+            searchEvents(text, "date", (orderByDate) ? DESC_ORDER_BY : ASC_ORDER_BY).then((res: any) => {
                 setEvents(res.data);
             })
         }, 500);
@@ -59,7 +61,7 @@ const SearchEvent = () => {
 
     const orderBy = (active: boolean) => {
         setOrderByDate(active);
-        searchEvents(search, "date", (active) ? ASC_ORDER_BY : DESC_ORDER_BY).then((res:any) => {
+        searchEvents(search, "date", (active) ? ASC_ORDER_BY : DESC_ORDER_BY).then((res: any) => {
             setEvents(res.data);
         })
     }
@@ -67,11 +69,11 @@ const SearchEvent = () => {
     return (
         <>
             <MySeo title="Ticketea! Compra y vende entradas de manera segura"
-                   description="Compra la entrada al evento que quieres asistir de manera segura con nuestros vendedores verificados rapido y facil"
-                   link="https://ticketea.me/"
-                   image="/images/logo.png"></MySeo>
+                description="Compra la entrada al evento que quieres asistir de manera segura con nuestros vendedores verificados rapido y facil"
+                link="https://ticketea.me/"
+                image="/images/logo.png"></MySeo>
             <Box className={styles.parent}>
-                <Box className={styles.background} style={{backgroundImage: "url(/images/party-banner-"+imageNumber+".jpg)"}}></Box>
+                <Box className={styles.background} style={{ backgroundImage: "url(/images/party-banner-" + imageNumber + ".jpg)" }}></Box>
                 <Box paddingBottom={"120px"} paddingTop={"100px"} paddingLeft={"15px"} paddingRight={"15px"}>
                     <Center textAlign={"center"}>
                         <VStack>
@@ -83,44 +85,86 @@ const SearchEvent = () => {
                     </Center>
                 </Box>
             </Box>
-            <Box marginTop={{"base": "-75px", "sm": "-75px", "customMd": "-90px"}} padding={{"base": "40px 1.5rem", "sm": "40px 1.5rem", "customMd": "40px 250px", "customLg": "40px 350px", "customXl": "40px 450px"}}>
+            <Box marginTop={{ "base": "-75px", "sm": "-75px", "customMd": "-90px" }} padding={{ "base": "40px 1.5rem", "sm": "40px 1.5rem", "customMd": "40px 250px", "customLg": "40px 350px", "customXl": "40px 450px" }}>
                 <MyBigInputSearch value={search} setValue={inputChanged}></MyBigInputSearch>
+                
+                <Carousel
+                    arrows={false} 
+                    showDots={false}
+                    autoPlay
+                    autoPlaySpeed={3000}
+                    centerMode={true}
+                    dotListClass=""
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    minimumTouchDrag={80}
+                    pauseOnHover
+                    responsive={{
+                        desktop: {
+                            breakpoint: {
+                                max: 3000,
+                                min: 1024
+                            },
+                            items: 3,
+                            partialVisibilityGutter: 40
+                        }
+                    }}
+                    rewind={false}
+                    rewindWithAnimation={false}
+                    rtl={false}
+                    shouldResetAutoplay
+                    slidesToSlide={2}
+                    swipeable
+                >
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </Carousel>
+
                 {
-                    (events.length) ? 
-                    <>
-                        <Box className={styles.containerTitle}>
-                            <HStack justify={"space-between"}>
-                                <SectionTitle title="Eventos encontrados"></SectionTitle>
-                                <OrderByMenu onChange={(active)=>{orderBy(active)}} 
-                                             text="Ordenar por: Fecha"
-                                             default={true}></OrderByMenu>
-                            </HStack>
+                    (events.length) ?
+                        <>
+                            <Box className={styles.containerTitle}>
+                                <HStack justify={"space-between"}>
+                                    <SectionTitle title="Eventos"></SectionTitle>
+                                    <OrderByMenu onChange={(active) => { orderBy(active) }}
+                                        text="Ordenar por: Fecha"
+                                        default={true}></OrderByMenu>
+                                </HStack>
+                            </Box>
+                            <Box>
+                                <VStack align='stretch' spacing={3}>
+                                    {
+                                        events.map((event: EventDate, index: any) => (
+                                            <EventCard key={index}
+                                                eventImage={event.event.image_url}
+                                                eventName={event.event.title}
+                                                artistName={event.event.artist.name}
+                                                ticketsNumber={event.ticketsCount}
+                                                eventDate={event.date}
+                                                eventId={event.encId}
+                                                onClick={() => { navigate("/tickets/" + event.encId) }}></EventCard>
+                                        ))
+                                    }
+                                </VStack>
+                            </Box>
+                        </> :
+                        <Box className={styles.containerNoResults}>
+                            <span className={styles.noResultsTitle}>
+                                Hay más de 200 eventos en el Perú al año (conciertos, partidos, stand up, etc) y con <Text fontFamily={"montserratBold"} display={"inline-block"}>Ticketea</Text> podrás ir a cada uno de ellos de manera segura y sin preocuparte de las estafas
+                            </span>
                         </Box>
-                        <Box>
-                            <VStack align='stretch' spacing={3}>
-                                {
-                                    events.map((event: EventDate, index: any) => (
-                                        <EventCard key={index} 
-                                            eventImage={event.event.image_url}
-                                            eventName={event.event.title}
-                                            artistName={event.event.artist.name}
-                                            ticketsNumber={event.ticketsCount}
-                                            eventDate={event.date}
-                                            eventId={event.encId}
-                                            onClick={()=>{navigate("/tickets/" + event.encId)}}></EventCard>
-                                    ))
-                                }
-                            </VStack>
-                        </Box>
-                    </> : 
-                    <Box className={styles.containerNoResults}>
-                        <span className={styles.noResultsTitle}>
-                            Hay más de 200 eventos en el Perú al año (conciertos, partidos, stand up, etc) y con <Text fontFamily={"montserratBold"} display={"inline-block"}>Ticketea</Text> podrás ir a cada uno de ellos de manera segura y sin preocuparte de las estafas
-                        </span>
-                    </Box>
                 }
             </Box>
-            
+
         </>
     );
 };
